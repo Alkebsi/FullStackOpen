711bfcd610e5354b5d281871bd02ba4881141997
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-10-19 02:16:20 -0700

    Exercise 2.6 Prompt:  Start by implementing the addition of a person to the phonebook Note: I added the id since it way just easy to do so right now.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,48 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+      id: 1,
+    },
+  ]);
   const [newName, setNewName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const nameObj = {
+      name: newName,
+      id: persons.length + 1,
+    };
+
+    setPersons(persons.concat(nameObj));
+    setNewName('');
+  };
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNoteChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => {
+        return <p key={person.id}>
+          {person.name} <br />
+        </p>
+      })}
     </div>
   );
 };
