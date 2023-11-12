b7a63ec506696b630bf072f4e14d1538e6dc420f
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-12 13:04:46 +0300

    EX 4.2 Implemented the blogs controller and model into their own modules

--- part4/app.js
+++ part4/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
-// const notesRouter = require('./controllers/notes')
+const blogsRouter = require('./controllers/blog');
 // const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 
@@ -11,33 +11,6 @@ const app = express();
 mongoose.set('strictQuery', false);
 
 logger.info(`Connecting to ${config.MONGODB_URI}`);
-
-// -- will go to there own modules soon --//
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-// -- will go to there own modules soon --//
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -51,6 +24,7 @@ mongoose
 app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
+app.use('/api/blogs', blogsRouter);
 // app.use(middleware.requestLogger);
 
 module.exports = app;
