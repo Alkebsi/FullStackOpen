4858858160843ed6bb7c65e7ddedb59b5fcd3b5b
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-12 14:05:54 +0300

    Exercise 4.4 Prompt: Define a new function that returns the total likes. Note: I have made a mistake in the previeus commit! The excercise number should have been 4.3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,13 @@
 const dummy = () => 1;
 
+const totalLikes = (bloglist) => {
+  const likes = bloglist.map((p) => p.likes);
+  const sum = (x, y) => x + y;
+
+  return likes.reduce(sum);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
