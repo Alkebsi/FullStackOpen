1556a21c87ed7df0120d643dcde6c0486a9c9b0b
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-12 12:35:06 +0300

    EX 4.2 Done with the configurations file for .env secrets

--- part4/index.js
+++ part4/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const logger = require('./utils/logger');
+const config = require('./utils/config');
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -15,8 +16,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb://localhost:27017/bloglist';
-mongoose.connect(mongoUrl);
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
@@ -35,7 +35,6 @@ app.post('/api/blogs', (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
