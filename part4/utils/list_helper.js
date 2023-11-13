e977ba3b09f9113e34a50345b0dbbf8c334f4588
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-13 14:36:11 +0300

    Exercise 4.17 Prompt: Define a function that receives an array of blogs and, returns the author, whose blog posts have the largest amount of likes. Note: Used the old algrothm and didn't had spend that much on this one!

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -28,20 +28,39 @@ const favoriteBlog = (bloglist) => {
 
 // Input: blogs, Output: author with most blogs + post's number
 const mostBlogs = (bloglist) => {
-  let authors = bloglist.map((blog) => blog.author);
-  authors = [...new Set(authors)];
+  const authors = [...new Set(bloglist.map((blog) => blog.author))];
 
   const numberOfPosts = new Array(authors.length).fill(0);
 
   // I had to break the rules here, sorry!
   // eslint-disable-next-line no-return-assign
-  bloglist.map((p) => numberOfPosts[authors.indexOf(p.author)] += 1);
+  bloglist.map((p) => (numberOfPosts[authors.indexOf(p.author)] += 1));
 
-  const index = numberOfPosts.indexOf(Math.max(...numberOfPosts));
+  const i = numberOfPosts.indexOf(Math.max(...numberOfPosts));
 
   const result = {
-    author: authors[index],
-    blogs: numberOfPosts[index],
+    author: authors[i],
+    blogs: numberOfPosts[i],
+  };
+
+  return result;
+};
+
+// Input: blogs, Output: author with most liked post
+const mostLikes = (bloglist) => {
+  const authors = [...new Set(bloglist.map((p) => p.author))];
+
+  const sum = new Array(authors.length).fill(0);
+
+  // Sorry again to break the rules
+  // eslint-disable-next-line no-return-assign
+  bloglist.map((p) => sum[authors.indexOf(p.author)] += p.likes);
+
+  const i = sum.indexOf(Math.max(...sum));
+
+  const result = {
+    author: authors[i],
+    likes: sum[i],
   };
 
   return result;
@@ -52,4 +71,5 @@ module.exports = {
   totalLikes,
   favoriteBlog,
   mostBlogs,
+  mostLikes,
 };
