cb7fdcb5b6c42af9b2435bd30a18057a8de21b29
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-14 14:05:57 +0300

    Exercise 4.9 Prompt: Write a test that verifies that the is named 'id' not '_id'. Change the code as required to pass the test. Note: Done with it in a very long time, struggling! The linter is having a problem with the dangle underscors, so stoped it. It also have a problem with the func param assignment! Passed them all with an eslint-disable comment!

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -7,6 +7,17 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 });
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+    returnedObject.id = returnedObject._id.toString();
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+    delete returnedObject._id;
+    // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+    delete returnedObject.__v;
+  },
+});
+
 const Blog = mongoose.model('Blog', blogSchema);
 
 module.exports = Blog;
