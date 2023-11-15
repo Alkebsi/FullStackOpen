f1aff2fd548678633721c0dcc59eecdfda3c6601
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-15 09:26:14 +0300

    Exercise 4.10 Prompt: Write a test that verifies that making an HTTP POST request. Note: made sure that the request is fullfiled. The returned values are increased by one and the HTTP response is 201

--- part4/tests/backend.test.js
+++ part4/tests/backend.test.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
+const helper = require('./test_helper');
+
+// Making sure the database is equal to the inital blogs
+beforeEach(async () => {
+  await Blog.deleteMany();
+  let blogObject = new Blog(helper.initialBloglist[0]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBloglist[1]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBloglist[2]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBloglist[3]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBloglist[4]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBloglist[5]);
+  await blogObject.save();
+});
 
 const api = supertest(app);
 
@@ -15,6 +34,28 @@ test('blogs have a id than _id', async () => {
   const response = await api.get('/api/blogs');
 
   expect(response.body[0].id).toBeDefined();
+});
+
+test('a blog post can be added', async () => {
+  const newBlog = {
+    title: 'Getting Started with CEP Extensions',
+    author: 'Adobe Community',
+    url: 'https://github.com/Adobe-CEP/Getting-Started-guides',
+    likes: 10,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const response = await api.get('/api/blogs');
+
+  const contents = response.body.map((p) => p.title);
+
+  expect(response.body).toHaveLength(helper.initialBloglist.length + 1);
+  expect(contents).toContain('Getting Started with CEP Extensions');
 });
 
 afterAll(async () => {
