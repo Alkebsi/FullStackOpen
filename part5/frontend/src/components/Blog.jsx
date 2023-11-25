a826aebc9246bcbf0910e3f90cccb5cfd2eb9d9a
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-25 12:07:59 +0300

    Exercise 5.7 Prompt: Add a button to each blog to control its content visibility. Note: they mentioned that I can't achive the desired behavior using the Togglable component for some reason, yet it was possible. This commit holds the solution using the Togglable component. The commit after this one is what they asked for, using a state!

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useRef } from 'react';
+import Togglable from './Togglable';
 
+const Blog = ({ blog }) => {
+  const blogsRef = useRef();
+
+  return (
+    <div
+      style={{
+        border: '2px solid black',
+        borderRadius: '10px',
+        margin: '10px 0',
+        padding: '10px',
+      }}
+    >
+      {blog.title}
+      <Togglable buttonLable="view" ref={blogsRef}>
+        {blog.url}
+        <br />
+        likes {blog.likes} &nbsp;
+        <button>like</button>
+        <br />
+        {blog.author} &nbsp;
+      </Togglable>
+    </div>
+  );
+};
 export default Blog;
