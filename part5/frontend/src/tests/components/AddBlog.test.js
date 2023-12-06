c3e4288be178a6b843e39dc23313816b920e4884
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-12-06 13:14:19 -0800

    Exercise 5.16 Prompt: Make a test for the new blog form. When a new blog is created, the test should check the event handler and it props for containing the right information needed for a blog!

--- part5/frontend/src/tests/components/AddBlog.test.js
+++ part5/frontend/src/tests/components/AddBlog.test.js
@@ -9,31 +9,26 @@ test('adding a new blog works', async () => {
 
   // Mock Handlers
   const handleNewBlog = jest.fn();
-  const onTitleChange = jest.fn();
-  const onAuthorChange = jest.fn();
-  const onUrlChange = jest.fn();
-  let title = 'World War II';
-  let author = 'Johnson Drilla';
-  let url = 'https://ww2.com/doc';
 
-  const { container } = render(
-    <AddBlog
-      handleNewBlog={handleNewBlog}
-      onTitleChange={onTitleChange}
-      onAuthorChange={onAuthorChange}
-      onUrlChange={onUrlChange}
-      title={title}
-      author={author}
-      url={url}
-    />
-  );
+  const { container } = render(<AddBlog handleNewBlog={handleNewBlog} />);
 
-  const input = screen.getByPlaceholderText('Title here...');
-  await user.type(input, 'World War II');
+  const inputTitle = screen.getByPlaceholderText('Title here...');
+  await user.type(inputTitle, 'World War II');
+
+  const inputAuthor = screen.getByPlaceholderText('Author here...');
+  await user.type(inputAuthor, 'Rober Json');
+
+  const inputUrl = screen.getByPlaceholderText('URL here...');
+  await user.type(inputUrl, 'http://hello.world/');
+
   const submitButton = screen.getByText('create');
   await user.click(submitButton);
 
   expect(handleNewBlog.mock.calls).toHaveLength(1);
-  console.log(handleNewBlog.mock.calls.target);
-  // expect(handleNewBlog.mock.calls[0][0].content).toBe('World War II');
+  console.log(handleNewBlog.mock.calls);
+
+  expect(handleNewBlog.mock.calls).toHaveLength(1);
+  expect(handleNewBlog.mock.calls[0][0].title).toBe('World War II');
+  expect(handleNewBlog.mock.calls[0][0].author).toBe('Rober Json');
+  expect(handleNewBlog.mock.calls[0][0].url).toBe('http://hello.world/');
 });
