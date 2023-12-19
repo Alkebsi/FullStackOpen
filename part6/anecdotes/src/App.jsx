539953e2118e8c6ed5e635da7060b1ff586a1bb0
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-12-19 00:58:58 -0800

    Exercise 6.8 Prompt: Separate the rendering of anecdotes into another component.

--- part6/anecdotes/src/App.jsx
+++ part6/anecdotes/src/App.jsx
@@ -1,32 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { vote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
-const App = () => {
-  const anecdotes = useSelector((state) =>
-    state.sort((a, b) => b.votes - a.votes)
-  )
-  const dispatch = useDispatch()
-
-  const votes = (id) => {
-    dispatch(vote(id))
-  }
-
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => votes(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-      <AnecdoteForm />
-    </div>
-  )
-}
-
+const App = () => (
+  <div>
+    <h2>Anecdotes</h2>
+    <AnecdoteList />
+    <AnecdoteForm />
+  </div>
+)
 export default App
---
+++ part6/anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) =>
+    state.sort((a, b) => b.votes - a.votes)
+  )
+  const dispatch = useDispatch()
+
+  const votes = (id) => {
+    dispatch(vote(id))
+  }
+
+  return (
+    <>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => votes(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default AnecdoteList
