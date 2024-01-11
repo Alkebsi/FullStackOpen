be2190f803c0623b04ae11b3626936c18dbabea8
Alkebsi <moha.alkebsi@gmail.com>
2024-01-11 12:17:21 +0300

    Ex-6.21
    Done whats needed, yet not optimized

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,19 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
 
+const AnecdoteForm = () => {
+   
+  const queryClient = useQueryClient()
+  const newNoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['anecdotes'] }) 
+  })
+ 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    newNoteMutation.mutate({ content, votes: 0 })
     console.log('new anecdote')
 }
 
