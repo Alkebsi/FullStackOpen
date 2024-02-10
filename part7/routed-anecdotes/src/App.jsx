a098d1243115c22ffb52782498f439f56956709e
Alkebsi <moha.alkebsi@gmail.com>
2024-02-10 15:49:39 +0300

    Exercise 7.2
    Implement a view for showing a single anecdote.

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom'
 import { useState } from 'react'
 
 const Menu = () => {
@@ -18,10 +18,31 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => {
+        return (
+          <li key={anecdote.id} >
+            <Link to={`/anecdote/${anecdote.id}`}>
+              {anecdote.content}
+            </Link>
+          </li>
+        )
+      })}
     </ul>
   </div>
 )
+
+const Anecdote = ({ anecdotes }) => {
+  const id = useParams().id;
+  const { content, author, votes, info } = anecdotes.find((n) => n.id === Number(id))
+  
+  return (
+    <div>
+      <h2>{ content } by { author }</h2>
+      <p>has { votes } votes</p>
+      <p>for more info see <a href={ info }>{ info }</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -59,6 +80,8 @@ const CreateNew = (props) => {
       info,
       votes: 0
     })
+    
+    
   }
 
   return (
@@ -132,6 +155,7 @@ const App = () => {
           <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
           <Route path="/about" element={<About />} />
           <Route path="/create" element={<CreateNew addNew={addNew} />} />
+          <Route path="/anecdote/:id" element={<Anecdote anecdotes={anecdotes} />} />
         </Routes>
         <Footer />
       </Router>
